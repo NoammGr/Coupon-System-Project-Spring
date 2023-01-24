2e77a78a65c81ffe501817ed2158053eb47e7bc2
Noam Greedi <noamgreedi45@gmail.com>
2023-01-24 16:42:08 +0200

    ...

--- src/main/java/app/core/entities/Coupon.java
+++ src/main/java/app/core/entities/Coupon.java
@@ -1,10 +1,25 @@
 package app.core.entities;
 
-import lombok.*;
-
-import javax.persistence.*;
 import java.sql.Date;
 import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @Entity
